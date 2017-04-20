ff0ab0bf4e300156090eac88ff60e0d67a610f4b
Hack Reactor Student <student@hackreactor.com>
2017-04-19 21:15:08 -0700

    pass 8/9 test cases

--- helpers/archive-helpers.js
+++ helpers/archive-helpers.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 var path = require('path');
 var _ = require('underscore');
-
+var http = require('http');
 /*
  * You will need to reuse the same paths many times over in the course of this sprint.
  * Consider using the `paths` object below to store frequently used file paths. This way,
@@ -25,17 +25,59 @@ exports.initialize = function(pathsObj) {
 // The following function names are provided to you to suggest how you might
 // modularize your code. Keep it clean!
 
+
 exports.readListOfUrls = function(callback) {
+  fs.readFile(exports.paths.list, function(err, data) {
+    if (err) {
+      throw err;
+    }
+    var list = data.toString().split('\n');
+    console.log('READ LIST OF URLS: ', list);
+    callback(list);
+  });
 };
 
 exports.isUrlInList = function(url, callback) {
+  exports.readListOfUrls(function(urls) {
+    var isInList = _.contains(urls, url);
+    callback(isInList);
+  });
 };
 
 exports.addUrlToList = function(url, callback) {
+  exports.isUrlInList(url, function(exist) {
+    if (!exist) {
+      exports.readListOfUrls(function(list) {
+        list.push(url);
+        fs.writeFile(exports.paths.list, list.join('\n'));
+        callback();
+      });
+    }
+  });
 };
 
 exports.isUrlArchived = function(url, callback) {
+  fs.readdir(exports.paths.archivedSites, function(err, files) {
+    //files = ['fileA', 'fileB']
+    if (err) {
+      throw err;
+    }
+    var isInArch = _.contains(files, url);
+    callback(isInArch);
+  });
 };
 
 exports.downloadUrls = function(urls) {
+  _.each(urls, function(url) {
+    var file = fs.createWriteStream(exports.paths.archivedSites + '/' + url);
+    var request = http.get('http://' + url, function(response) {
+      response.pipe(file);
+      file.on('finish', function() {
+        file.close();
+      });
+    }).on('error', function(err) {
+      fs.unlink(url);
+      throw err;
+    });
+  });
 };
--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,37 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var fs = require('fs');
+var indexFile;
 // require more modules/folders here!
+fs.readFile(archive.paths.siteAssets + '/index.html', function(err, data) {
+  if (err) {
+    throw err;
+  }
+  indexFile = data;
+});
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  // res.end(archive.paths.list);
+  if (req.method === 'GET' && req.url === '/') {
+    // console.log('INIT');
+    res.end((indexFile).toString());
+  } else if (req.method === 'GET') {
+    // console.log('GET url: ', req.url);
+    archive.isUrlArchived(req.url.slice(1), function(exist) {
+      if (exist) {
+        fs.readFile(archive.paths.archivedSites + req.url, function(err, data) {
+          if (err) {
+            throw err;
+          }
+          res.end(data.toString());
+        });
+      } else {
+        res.statusCode = 404;
+        res.end();
+      }
+    });
+  } else if (req.method === 'POST') {
+    // console.log("POST url: ", Object.keys(req) );
+    archive.addUrlToList(req.url, function() {});
+  }
 };
